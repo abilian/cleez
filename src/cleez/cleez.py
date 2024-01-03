0b564541a9558cc4a8aacc673661e2991075040b
Stefane Fermigier <sf@fermigier.com>
2024-01-03 09:45:44 +0100

    feat: add 'run' function in the API.

--- src/cleez/cleez.py
+++ src/cleez/cleez.py
@@ -192,3 +192,10 @@ class CLI:
 
     def get_command_name(self):
         return self.name
+
+
+def run(module_name: str="", argv=None):
+    cli = CLI()
+    if module_name:
+        cli.scan(module_name)
+    cli.run(argv)
